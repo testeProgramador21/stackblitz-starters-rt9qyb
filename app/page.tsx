f5fbe649a4888f285065d945b5eb1cacdfad839a
testeProgramador21 <146689338+testeProgramador21@users.noreply.github.com>
2023-10-02 00:29:11 -0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+//SVG
+
+const Page = () => {
+  return(
+    <div className="container mx-auto">
+      <div className="svg">
+        
+      </div>
+
+    </div>
+  );
+}
+
+export default Page;
